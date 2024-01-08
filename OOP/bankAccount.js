3199bdec96e185a9fa4102cdccdf8dcef5119947
im-azad <me.azad99@gmail.com>
2024-01-08 23:03:20 +0600

    withdraw methods add

--- OOP/bankAccount.js
+++ OOP/bankAccount.js
@@ -3,12 +3,26 @@ class BankAccount {
     (this.accountNumber = accountNumber), (this.accountHolder = accountHolder);
     this.balance = balance;
   }
+
+  // deposit 
   deposit(amount) {
     if (amount > 0) {
       this.balance += amount;
-      console.log(`You deposited $${amount}. New blance is $${this.balance}`);
+      console.log(`You deposited $${amount}. New balance is $${this.balance}`);
     } else {
       console.log("Can't deposit a negative amount");
+    }
+  }
+
+  // withdraw
+  withdraw(amount) {
+    if(amount > this.balance) {
+      console.log("You can't withdraw that much!");
+    }
+    else {
+      this.balance -= amount
+      console.log(`You withdraw $${amount}. New balance is $${this.balance}`);
+
     }
   }
 
